498a3306f6f4be03a9588188a4a16283b49a2938
juandavidcruzgomez <jdcruz-gomez@umi.us>
2024-01-30 00:22:33 +0100

    First commit

---
+++ download-data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "Create directories..."
+mkdir -p data
+echo "Download the compressed files..."
+cd data
+wget https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-160.tgz -O ../data/imagenette2-160.tgz
+echo "Uncompress the raw data..."
+tar -xzf imagenette2-160.tgz
+mv imagenette2-160 raw
+touch raw/.datalock 
+echo "Cleaning up..."
+rm -f imagenette2-160.tgz
+echo "Done"
