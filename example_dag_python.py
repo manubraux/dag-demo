84ba90db0b045259e75da0371141ed261aad06bb
juandavidcruzgomez <jdcruz-gomez@umi.us>
2024-01-30 16:35:21 +0100

    Example file of a dag in Python

---
+++ example_dag_python.py
@@ -0,0 +1,35 @@
+import os
+import sys
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models.dag import DAG
+from airflow.decorators import task
+
+from datetime import timedelta
+
+@task(task_id="test_task")
+def simple_testask():
+    print(sys.path)
+    print("Hello, this is the test task")
+    return "Done"
+
+with DAG(
+    "sample_python_dag",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "email": ["ledatascientist@ds.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5)
+    },
+    description="A basic example DAG ",
+    catchup=False,
+    tags=["demo", "example", "test"],
+) as dag:
+    
+    test_task = simple_testask()
+
+
+    test_task
